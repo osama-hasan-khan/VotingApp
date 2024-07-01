b661e919557df6c0218dcd08ff5beb0af03c901b
osama-hasan-khan <osamahasankhan098@gmail.com>
2024-07-01 17:45:17 +0530

    create controllers for vote api

--- backend/controllers/voteController.js
+++ backend/controllers/voteController.js
@@ -1,4 +1,4 @@
-import Vote from "../models/Vote.js"
+import Vote from "../models/Vote.js";
 
 const createVote = async (req, res) => {
   try {
@@ -21,4 +21,70 @@ const createVote = async (req, res) => {
   }
 };
 
-export { createVote };
+const getVotes = async (req, res) => {
+  try {
+    const votes = await Vote.find();
+    res.status(200).json(votes);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+    console.log("Error in getting votes: ", err.message);
+  }
+};
+
+const getVoteById = async (req, res) => {
+  try {
+    const vote = await Vote.findById(req.params.id);
+
+    if (!vote) {
+      return res.status(404).json({ error: "Vote not found" });
+    }
+
+    res.status(200).json(vote);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+    console.log("Error getting vote by ID: ", err.message);
+  }
+};
+
+const updateVote = async (req, res) => {
+  try {
+    const { title, end_date, description, options } = req.body;
+
+    const updatedVote = await Vote.findByIdAndUpdate(
+      req.params.id,
+      {
+        title,
+        end_date,
+        description,
+        options,
+      },
+      { new: true }
+    );
+
+    if (!updatedVote) {
+      return res.status(404).json({ error: "Vote not found" });
+    }
+
+    res.status(200).json(updatedVote);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+    console.log("Error in updating vote: ", err.message);
+  }
+};
+
+const deleteVote = async (req, res) => {
+  try {
+    const deletedVote = await Vote.findByIdAndDelete(req.params.id);
+
+    if (!deletedVote) {
+      return res.status(404).json({ error: "Vote not found" });
+    }
+
+    res.status(200).json({ message: "Vote deleted successfully" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+    console.log("Error in deleting vote: ", err.message);
+  }
+};
+
+export { createVote, getVotes, updateVote, getVoteById, deleteVote };
--- backend/routes/voteRoutes.js
+++ backend/routes/voteRoutes.js
@@ -1,9 +1,19 @@
 import express from "express";
-import { createVote } from "../controllers/voteController.js";
+import {
+  createVote,
+  deleteVote,
+  getVoteById,
+  getVotes,
+  updateVote,
+} from "../controllers/voteController.js";
 import { authenticate } from "../middlewares/authorizationMiddleware.js";
 
 const router = express.Router();
 
 router.post("/createVote", authenticate, createVote);
+router.get("/getVotes", authenticate, getVotes);
+router.get("/getVoteById/:id", authenticate, getVoteById);
+router.put("/updateVote/:id", authenticate, updateVote);
+router.delete("/deleteVote/:id", authenticate, deleteVote);
 
 export default router;
